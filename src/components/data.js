5bd82d63284d3f1eacf0dbaa7ed4dce7a3cc742f
ANNA SCARIA PALLIVATHUKAL <annascaria>
2024-07-24 19:38:44 +0530

    Single selection

---
+++ src/components/data.js
@@ -0,0 +1,25 @@
+const data = [
+{
+
+    id : '1',
+    question : "NAME ?" ,
+    answer : "Anna Scaria"
+},
+{
+    id : '2',
+    question : "AGE ?" ,
+    answer : "25",
+},
+{
+    id : '3',
+    question : "BLOOD GP ?" ,
+    answer : "o+",
+},
+{
+    id : '4',
+    question : "SEX ?" ,
+    answer : "Female",
+},
+];
+
+export default data;
